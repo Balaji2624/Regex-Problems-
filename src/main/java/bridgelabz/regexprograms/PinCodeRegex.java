9bf7678a301bae8e00ce28ba1a1287b376aa7b08
Balaji2624 <balajisapkal3062001@gmail.com>
2024-09-13 21:12:49 +0530

    Added: Make sure 400 088 is also valid along with 400088.

--- src/main/java/bridgelabz/regexprograms/PinCodeRegex.java
+++ src/main/java/bridgelabz/regexprograms/PinCodeRegex.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PinCodeRegex {
-    private static final String PINCODE_PATTERN = "^[1-9][0-9]{5}$";
+    private static final String PINCODE_PATTERN = "^[1-9][0-9]{2}\\s?[0-9]{3}$";
     public static boolean validatePinCode(String pincode){
         if (!Character.isDigit(pincode.charAt(0))) {
             return false;
@@ -18,11 +18,17 @@ public class PinCodeRegex {
     }
 
     public static void main(String[] args) {
-        String pincodeToValidate = "400088B";
-
-        if(validatePinCode(pincodeToValidate)){
-            System.out.println(pincodeToValidate+ " is valid pincode.");
+        String pincodeToValidate1 = "400088";
+        String pincodeToValidate2 = "400 088";
+        if(validatePinCode(pincodeToValidate1)){
+            System.out.println(pincodeToValidate1+ " is valid pincode.");
         }else{
+            System.out.println("Invalid Pincode..!");
+        }
+
+        if (validatePinCode(pincodeToValidate2)) {
+            System.out.println(pincodeToValidate2 + " is a valid pincode.");
+        } else {
             System.out.println("Invalid Pincode..!");
         }
     }
