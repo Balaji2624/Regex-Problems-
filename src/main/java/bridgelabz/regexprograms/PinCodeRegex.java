1189d227e9a8aba0ca71d60cd473ae9f8fc995a9
Balaji2624 <balajisapkal3062001@gmail.com>
2024-09-13 20:43:29 +0530

    Added: Restrct the pin code from strating with Alphabet

--- src/main/java/bridgelabz/regexprograms/PinCodeRegex.java
+++ src/main/java/bridgelabz/regexprograms/PinCodeRegex.java
@@ -6,13 +6,16 @@ import java.util.regex.Pattern;
 public class PinCodeRegex {
     private static final String PINCODE_PATTERN = "^[1-9][0-9]{5}$";
     public static boolean validatePinCode(String pincode){
+        if (!Character.isDigit(pincode.charAt(0))) {
+            return false;
+        }
         Pattern pattern = Pattern.compile(PINCODE_PATTERN);
         Matcher matcher = pattern.matcher(pincode);
         return matcher.matches();
     }
 
     public static void main(String[] args) {
-        String pincodeToValidate = "400088";
+        String pincodeToValidate = "A400088";
 
         if(validatePinCode(pincodeToValidate)){
             System.out.println(pincodeToValidate+ " is valid pincode.");
