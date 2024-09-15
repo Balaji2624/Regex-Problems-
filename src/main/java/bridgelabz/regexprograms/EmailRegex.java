927dabc88a2fdd1c224c27e0a72e54036f21ada7
Balaji2624 <balajisapkal3062001@gmail.com>
2024-09-15 16:14:17 +0530

    Added: handle optional part i.e. xyz in abc.xyz@bridgelabz.co.in

--- src/main/java/bridgelabz/regexprograms/EmailRegex.java
+++ src/main/java/bridgelabz/regexprograms/EmailRegex.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EmailRegex {
-    private static final String EMAIL_PATTERN = "^[a-z]{3}\\.?[a-z]*@[a-z]{10}\\.[a-z]{2}.*$";
+    private static final String EMAIL_PATTERN = "^[a-z]{3}([_+\\-.][a-z]+)?@[a-z]{10}\\.[a-z]{2}.*$";
 
     public static boolean validateEmail(String email) {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
@@ -13,12 +13,16 @@ public class EmailRegex {
     }
 
     public static void main(String[] args) {
-        String emailToValidate = "abc.xyz@bridgelabz.co.in";
+        String emailToValidate2 = "abc.xyz@bridgelabz.co.in";
+        String emailToValidate1 = "abc.v@bridgelabz.co.in";
+        String emailToValidate3 = "abc+xyz@bridgelabz.co.in";
+        String emailToValidate4 = "abc-xyz@bridgelabz.co.in";
+        String emailToValidate5 = "abc#xyz@bridgelabz.co.in";
 
-        if (validateEmail(emailToValidate)) {
-            System.out.println(emailToValidate + " has a valid start with abc and bridgelabz .");
-        } else {
-            System.out.println(emailToValidate + " does not have a valid start (abc).");
-        }
+        System.out.println(emailToValidate1 + ": " + (validateEmail(emailToValidate1) ? "Valid" : "Invalid"));
+        System.out.println(emailToValidate2 + ": " + (validateEmail(emailToValidate2) ? "Valid" : "Invalid"));
+        System.out.println(emailToValidate3 + ": " + (validateEmail(emailToValidate3) ? "Valid" : "Invalid"));
+        System.out.println(emailToValidate4 + ": " + (validateEmail(emailToValidate4) ? "Valid" : "Invalid"));
+        System.out.println(emailToValidate5 + ": " + (validateEmail(emailToValidate5) ? "Valid" : "Invalid"));
     }
 }
