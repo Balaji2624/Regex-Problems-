1af61e8851a940ca4539c4f6827af409542a0a08
Balaji2624 <balajisapkal3062001@gmail.com>
2024-09-15 16:33:38 +0530

    Added: close the expression with country code supporting optional parts.

--- src/main/java/bridgelabz/regexprograms/EmailRegex.java
+++ src/main/java/bridgelabz/regexprograms/EmailRegex.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EmailRegex {
-    private static final String EMAIL_PATTERN = "^[a-z]{3}([_+\\-.][a-z]+)?@[a-z]{10}\\.[a-z]{2}.*$";
+    private static final String EMAIL_PATTERN = "^[a-z]{3}([_+\\-.][a-z]+)?@[a-z]{10}\\.[a-z]{2}.(\\.[a-z]{2})?$";
 
     public static boolean validateEmail(String email) {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
@@ -13,16 +13,15 @@ public class EmailRegex {
     }
 
     public static void main(String[] args) {
-        String emailToValidate2 = "abc.xyz@bridgelabz.co.in";
-        String emailToValidate1 = "abc.v@bridgelabz.co.in";
-        String emailToValidate3 = "abc+xyz@bridgelabz.co.in";
-        String emailToValidate4 = "abc-xyz@bridgelabz.co.in";
-        String emailToValidate5 = "abc#xyz@bridgelabz.co.in";
+        String emailToValidate1 = "abc@bridgelabz.co.in";      // Valid, with optional country code
+        String emailToValidate2 = "abc.xyz@bridgelabz.co";     // Valid, without country code
+        String emailToValidate3 = "abc.xyz@bridgelabz.co.uk";  // Valid, with optional country code
+        String emailToValidate4 = "abc.xyz@bridgelabz";
 
         System.out.println(emailToValidate1 + ": " + (validateEmail(emailToValidate1) ? "Valid" : "Invalid"));
         System.out.println(emailToValidate2 + ": " + (validateEmail(emailToValidate2) ? "Valid" : "Invalid"));
         System.out.println(emailToValidate3 + ": " + (validateEmail(emailToValidate3) ? "Valid" : "Invalid"));
         System.out.println(emailToValidate4 + ": " + (validateEmail(emailToValidate4) ? "Valid" : "Invalid"));
-        System.out.println(emailToValidate5 + ": " + (validateEmail(emailToValidate5) ? "Valid" : "Invalid"));
+    
     }
 }
